823675b24d187766e7655da17dc78b6913b27245
jwnpoh <jwn.poh@gmail.com>
2020-12-19 23:34:03 +0800

    Add color output

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module "gitlab.com/jwnpoh/terminal greeter"
 
 go 1.15
+
+require (
+	github.com/logrusorgru/aurora v2.0.3+incompatible
+	gopkg.in/logrusorgru/aurora.v1 v1.0.0-20181002194514-a7b3b318ed4e // indirect
+)
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/logrusorgru/aurora"
+	"math/rand"
 	"os/user"
 	"time"
 )
@@ -12,14 +14,36 @@ func main() {
 
 	u, _ := user.Current()
 
+	rand.Seed(time.Now().UnixNano())
+
+	var msg string
+
 	switch h := time.Now().Hour(); {
 	case h >= 18: // 18 to 23
-		fmt.Printf("Good evening, %s!\n", u.Username)
+		msg = "\rGood evening, %s!\n"
 	case h >= 12: // 12 to 18
-		fmt.Printf("Good afternoon, %s!\n", u.Username)
+		msg = "\rGood afternoon, %s!\n"
 	case h >= 6: // 6 to 11
-		fmt.Printf("Good morning, %s!\n", u.Username)
+		msg = "\rGood morning, %s!\n"
 	default: // 0 to 5
-		fmt.Printf("Good morning, %s! Why are you not sleeping??\n", u.Username)
+		msg = "\rGood morning, %s! Why are you not sleeping??\n"
+	}
+
+	r := rand.Intn((6))
+	switch r {
+	case 1:
+		fmt.Print(aurora.Sprintf(aurora.Red(msg), aurora.Red(u.Username)))
+	case 2:
+		fmt.Print(aurora.Sprintf(aurora.Green(msg), aurora.Green(u.Username)))
+	case 3:
+		fmt.Print(aurora.Sprintf(aurora.Blue(msg), aurora.Blue(u.Username)))
+	case 4:
+		fmt.Print(aurora.Sprintf(aurora.Yellow(msg), aurora.Yellow(u.Username)))
+	case 5:
+		fmt.Print(aurora.Sprintf(aurora.Magenta(msg), aurora.Magenta(u.Username)))
+	case 6:
+		fmt.Print(aurora.Sprintf(aurora.Cyan(msg), aurora.Cyan(u.Username)))
+	case 0:
+		fmt.Print(aurora.Sprintf(aurora.White(msg), aurora.White(u.Username)))
 	}
 }
