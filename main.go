842feca0bab5c0bb1ab24ea577924dcb05732f8d
jwnpoh <jwn.poh@gmail.com>
2021-01-01 23:06:30 +0800

    Added code for infinite loop (commented out)

--- main.go
+++ main.go
@@ -18,6 +18,19 @@ func main() {
 
 	var msg string
 
+	/* for infinite random color loop with carriage return
+	switch h := time.Now().Hour(); {
+	case h >= 18: // 18 to 23
+		msg = "\rGood evening, %s!"
+	case h >= 12: // 12 to 18
+		msg = "\rGood afternoon, %s!"
+	case h >= 6: // 6 to 11
+		msg = "\rGood morning, %s!"
+	default: // 0 to 5
+		msg = "\rGood morning, %s! Why are you not sleeping??"
+	}
+	*/
+
 	switch h := time.Now().Hour(); {
 	case h >= 18: // 18 to 23
 		msg = "\rGood evening, %s!\n"
@@ -29,6 +42,7 @@ func main() {
 		msg = "\rGood morning, %s! Why are you not sleeping??\n"
 	}
 
+	// for {
 	r := rand.Intn((6))
 	switch r {
 	case 1:
@@ -46,4 +60,6 @@ func main() {
 	case 0:
 		fmt.Print(aurora.Sprintf(aurora.White(msg), aurora.White(u.Username)))
 	}
+	//time.Sleep(250 * time.Millisecond)
+	//}
 }
